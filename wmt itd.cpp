6f9904e7c64783d6416d8616dc09f691852d3220
jadcudz <73225658+jadcudz@users.noreply.github.com>
2020-12-06 20:42:42 +0100

    Update wmt itd.cpp

--- wmt itd.cpp
+++ wmt itd.cpp
@@ -1,6 +1,27 @@
  
 #include <iostream>
 
+/**
+* \defgroup test1 TEST (Jakaś nazwa grupy)
+* \(
+*/
+
+/** \brief Przykładowy Numer ID */
+#define NUMER_ID 41
+
+/**
+* \brief Funkcja main z dwoma argumentami
+*
+* \param[in] op1 - 
+*   parametr pierwszy argc jako liczba całkowita.
+* \param[in] op2 - 
+*   parametr drugi argv jako znakowa
+*
+* \return
+*    - 0 => zwracana wartość w każdym przypadku
+*/	
+
+
   int main(int argc , char * argv[]) {
 	return 0;
    }
